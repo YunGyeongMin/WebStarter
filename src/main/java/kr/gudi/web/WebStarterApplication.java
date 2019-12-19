9a7bf878a5a3e1d349f0ace8bc6fc3b3db0955eb
KimKyeongBean <kkb@goodee.co.kr>
2019-12-19 11:00:39 +0900

    프로젝트 기본

---
+++ src/main/java/kr/gudi/web/WebStarterApplication.java
@@ -0,0 +1,13 @@
+package kr.gudi.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/gudi/web/WebStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gudi.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
